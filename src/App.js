2007899535ed6cae85e9a936e6e7dc9d0ba4c812
andyvdoan <andyy.doan@gmail.com>
2020-11-12 17:11:15 -0600

    Added preloaded styles, components, and boilerplates for jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+/////////////////////
+// Global Imports //
+///////////////////
+import React from 'react';
+
+////////////////////
+// Styles Import //
+//////////////////
+import './styles/app.scss';
+
+////////////////////////
+// Component Imports //
+//////////////////////
+import Player from './components/Player';
+import Song from './components/Song';
+import Player from './components/Player';
+// import Nav from './components/Nav';
+// import Library from './components/Library';
+// import LibrarySong from './components/LibrarySong';
+
+//////////////////
+// Data Import //
+////////////////
+import chillhop from './data';
+
+/////////////////////
+// Utility Import //
+///////////////////
+// import { playAudio } from ".util";
+
 
 function App() {
   return (
+
+    // const [songs, setSongs] = useState(chillhop());
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Player />
+      <Song />
     </div>
   );
 }
---
+++ src/components/Song.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Song = () => {
+  return (
+    <div className="song-container">
+      <img src={currentSong.cover}></img>
+      <h1>Song Name</h1>
+      <h1>Artist</h1>
+    </div>
+    
+  )
+}
+
+export default Song;
