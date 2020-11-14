534914c2e9782a1eff04d70c473438aa6f1d09bf
andyvdoan <andyy.doan@gmail.com>
2020-11-14 00:44:42 -0600

    Included audio player functionality in main app

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ import './styles/app.scss';
 import Player from './components/Player';
 import Song from './components/Song';
 import Player from './components/Player';
-// import Nav from './components/Nav';
-// import Library from './components/Library';
-// import LibrarySong from './components/LibrarySong';
+import Nav from './components/Nav';
+import Library from './components/Library';
+import LibrarySong from './components/LibrarySong';
 
 //////////////////
 // Data Import //
@@ -26,17 +26,65 @@ import chillhop from './data';
 /////////////////////
 // Utility Import //
 ///////////////////
-// import { playAudio } from ".util";
+import { playAudio } from ".util";
 
 
 function App() {
+///////////////////////////////
+// Setting Audio  Reference //
+/////////////////////////////
+
+  const audioRef = useRef(null);
+
+///////////////////////////////////
+// Setting - Audio Player State //
+/////////////////////////////////
+
   const [songs, setSongs] = useState(chillhop());
   const [currentSong, setCurrentSong] = useState(songs[0]);
-  
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+    animationPercentage: 0,
+    volume: 0,
+  });
+
+/////////////////////////////////////
+// Updating -  Audio Player State //
+///////////////////////////////////
+  const [libraryStatus, setLibraryStatus] = useState(false);
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currenTime;
+    const duration = e.target.duration;
+
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const percentage = Math.round((roundedCurrent / roundedDuration) * 100);
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration: duration,
+      animationPercentage: percentage,
+      volume: e.target.volume,
+    });
+  };
+////////////////////////////////
+// End of Song Functionality //
+//////////////////////////////
+  const songEndHandler = async () => {
+    let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+    await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+    playAudio(isPlaying, audioRef);
+    return;
+  }
+
   return (
     <div className="App">
-      <Player />
+      <Nav />
       <Song />
+      <Player />
+      <Library />
+      <Player />
     </div>
   );
 }
